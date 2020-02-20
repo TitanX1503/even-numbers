3aa55aaaa49b84853e0a256eb9950e123e2d851a
Sebastian Hernandez <titanx1503@gmail.com>
2020-02-20 12:58:14 -0700

    evenNumbers

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace even_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i <= 10; i++)
+            {  //parte 1   parte2    parte 3
+                
+
+                if(i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+
+            }
+        }
+    }
+}
